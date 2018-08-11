035786d728da6fd894f35f64a4857a4f77134a1e
Falco Nogatz <fnogatz@gmail.com>
2018-08-11 12:13:26 +0200

    Add support for clocker.get(Date)

--- test/index.js
+++ test/index.js
@@ -31,7 +31,7 @@ test('status', function (t) {
   })
 })
 
-test('get', function (t) {
+test('get (no parameter)', function (t) {
   var clocker = initialize()
 
   clocker.start('some', function () {
@@ -44,6 +44,36 @@ test('get', function (t) {
 
       clocker.close(function () {
         t.end()
+      })
+    })
+  })
+})
+
+test('get (Date parameter)', function (t) {
+  var clocker = initialize()
+
+  var date1 = new Date('2018-01-01')
+  var date2 = new Date('2018-02-02')
+  clocker.start('some1', date1, function () {
+    clocker.start('some2', date2, function () {
+      clocker.get(date1, function (err, entry) {
+        t.notOk(err)
+
+        t.deepEqual(entry, {
+          type: 'some1'
+        })
+
+        clocker.get(date2, function (err, entry) {
+          t.notOk(err)
+
+          t.deepEqual(entry, {
+            type: 'some2'
+          })
+
+          clocker.close(function () {
+            t.end()
+          })
+        })
       })
     })
   })
