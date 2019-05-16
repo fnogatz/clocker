347cf995c6987def451dba8270ccd6861058e08a
Benjamin Rupp <github@benjaminrupp.de>
2019-05-16 20:58:43 +0200

    Extract toStamp function

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,3 +1,5 @@
+const {toStamp} = require('../lib/util')
+
 const test = require('tape')
 const formatElapsed = require('../lib/util').formatElapsed
 
@@ -11,6 +13,20 @@ test('formatElapsed', (t) => {
     t.test(`${elapsedSeconds} => ${expectedStringRepresentation}`, (t) => {
       const result = formatElapsed(elapsedSeconds)
       t.equal(result, expectedStringRepresentation)
+      t.end()
+    })
+  })
+})
+
+test('toStamp', (t) => {
+  const testCases = [
+    ['time!2018-03-13 07:15:00', 1520921700],
+    ['time!2019-05-16 21:05:00', 1558033500]
+  ]
+  testCases.forEach(([stringWithTimestamp, expectedEpochSeconds]) => {
+    t.test(`${stringWithTimestamp} => ${expectedEpochSeconds}`, (t) => {
+      const result = toStamp(stringWithTimestamp)
+      t.equal(result, expectedEpochSeconds)
       t.end()
     })
   })
