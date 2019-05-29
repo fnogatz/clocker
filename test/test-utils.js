64992aac31d8fa23fb6a104c575234cca5c5b89e
Benjamin Rupp <github@benjaminrupp.de>
2019-05-29 22:09:43 +0200

    Extract initializeDB helper function for CLI tests

--- test/test-utils.js
+++ test/test-utils.js
@@ -2,18 +2,21 @@ const path = require('path')
 const rimraf = require('rimraf')
 const Clocker = require('../lib/index')
 
+function initializeClocker () {
+  const dataDir = initializeDB()
+  return new Clocker({
+    dir: dataDir
+  })
+}
+
+function initializeDB () {
+  const dataDir = path.join(__dirname, 'datadir')
+  // Empty db dir
+  rimraf.sync(path.join(dataDir, 'db'))
+  return dataDir
+}
+
 module.exports = {
-  initializeClocker () {
-    var dataDir = path.join(__dirname, 'datadir')
-
-    // Empty db dir
-    rimraf.sync(path.join(dataDir, 'db'))
-
-    // Initialize clocker
-    var clocker = new Clocker({
-      dir: dataDir
-    })
-
-    return clocker
-  }
+  initializeClocker,
+  initializeDB
 }
