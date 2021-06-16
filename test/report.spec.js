fd7a9498776b19b3b05936391c1f6d8bd05f5cd3
Falco Nogatz <fnogatz@gmail.com>
2021-06-16 18:54:37 +0200

    Update dep strftime from v0.8 to v0.10; Fix problem with new Date() creation

--- test/report.spec.js
+++ test/report.spec.js
@@ -6,7 +6,7 @@ test('report', t => {
   t.plan(1)
   t.test('verbose mode', t => {
     const wrapper = createCLIWrapper()
-    const today = strftime('%Y-%m-%d', Date())
+    const today = strftime('%Y-%m-%d', new Date())
     wrapper.run(['add', today + ' 08:00:00', today + ' 10:30:00', '-m', 'Sample message.'])
     const reportResult = wrapper.run(['report', '-v'])
     t.ok(reportResult.stdout.includes('Sample message.'))
